a525803589fd3cf9382660e106777c1ca9751c07
Jakthi Dyo Eliandi <alfaawetalfa@gmail.com>
2025-05-28 14:02:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ html2canvas(document.querySelector("#sign")).then(canvas => {
     link.download = 'rambu-rambu.jpg';
     link.href = canvas.toDataURL("image/jpeg");
     document.body.appendChild(link);
-    link.textContent = "Klik untuk mengunduh gambar";
+    link.textContent = "download";
 });
